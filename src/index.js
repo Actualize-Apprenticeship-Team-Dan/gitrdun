c99bfd26e43ac3a42f91ccadcbae26d19f4579dc
heydanhey <danhey@gmail.com>
2018-07-30 22:24:48 -0500

    update dotenv prod config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') { require("dotenv").config(); }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
